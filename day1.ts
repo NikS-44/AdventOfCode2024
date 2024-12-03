eb89ec9afcad60ea954c184542dc7147a1de13ec
Nik Shah <nikshahee@gmail.com>
2024-12-02 17:36:28 -0700

    feat: day1 part 2

--- day1.ts
+++ day1.ts
@@ -21,6 +21,28 @@ const processLists = (list1: number[], list2: number[]) => {
     console.log(result)
 }
 
+const processLists2 = (list1: number[], list2: number[]) => {
+    list1.sort((a, b) => a - b)
+
+    const list2Map = new Map();
+    list2.map((val) => {
+        const currVal = list2Map.get(val);
+        if(currVal === undefined){
+            list2Map.set(val, 1)
+        } else {
+            list2Map.set(val, currVal + 1 );
+        }
+    })
+
+    const result = list1.reduce((acc, val, index) => {
+            const occurrences = list2Map.get(val);
+            return occurrences ? acc + occurrences * val : acc
+        }
+    , 0)
+
+    console.log(result)
+}
+
 const inputString =
     "3   4\n" +
     "4   3\n" +
@@ -31,4 +53,5 @@ const inputString =
 
 const {list1, list2} = processInput(inputString);
 
-processLists(list1, list2);
+// processLists(list1, list2);
+processLists2(list1, list2);
